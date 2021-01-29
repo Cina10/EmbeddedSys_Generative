ec832aa19b2a6a6a9f5d90968ca9331bcf489f3a
Cina10 <li.energy4@gmail.com>
2021-01-29 18:00:14 -0500

    light colors

--- weather_led.py
+++ weather_led.py
@@ -1,5 +1,10 @@
 import json
 import requests
+import board
+import neopixel 
+import time
+
+pixels = neopixel.NeoPixel(board.D21, 8, brightness = 0.1, auto_write = False, pixel_order=neopixel.RGBW)
 
 # make a env file to store your API key!
 API_KEY = None
@@ -12,7 +17,10 @@ with open("env.json", 'r') as env:
 # just hard coded some cities for testing purposes
 cities = {"shelburne": "zip=01370,us", "new_york": "zip=10027,us", "kunming": "q=kunming,cn", "london": "q=london,uk"}
 request = 'http://api.openweathermap.org/data/2.5/weather?{}&units=imperial&appid={}'.format(cities["new_york"], API_KEY)
-print(request)
+icon = ""
+temp = 0
+wind_speed = 0
+
 response = requests.get(request)
 if response.status_code == 200:
     city_weather_dict = response.json()
@@ -21,7 +29,8 @@ if response.status_code == 200:
     # using the icons as they provide the most standardized info across weather types and day/night status
     # 01, 02, 03, 04, 09, 10, 11, 13, 50 icon codes from least to most overcast
     icon = city_weather_dict['weather'][0]['icon']
-
+    
+    print(icon)
     # temperature modulates color -- cooler temp = cooler tones
     # temperatures returned in fahrenheit
     temp = city_weather_dict['main']['temp']
@@ -29,3 +38,34 @@ if response.status_code == 200:
     # wind_speed modulates rate at which the lights change
     wind_speed = city_weather_dict['wind']['speed']
 
+# returns a list of colors
+def calculate_colors(temp):
+    color_list = []
+    # warm tones modulate G140->215 R215 B8
+    if temp > 70 :
+        b = 8
+        
+    # cool tones modulate red and green 
+    elif temp > 40:
+        b = 8
+    
+    # cold tones modulate blue
+    else:
+        b=8 
+
+colors = calculate_colors(temp)
+while True:
+   shift = 0
+   g = 120
+   r = 235
+   b = 8
+   for i in range(8):
+       pixels[i] = (g, r, b, 255)
+       r-=20*i//8
+       b+=246//8
+       
+    pixels.show()
+    time.sleep(0.005)
+        
+   
+    
